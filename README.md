79069c7198e94a009727effdbf1bdbd3e0a33801
Toanphuoc2611 <toanphuoc26112003@gmail.com>
2023-11-12 19:49:42 +0700

    chuyển đổi html sang file jsp

--- README.md
+++
@@ -1,2 +0,0 @@
-# WebProject
-do an web
---
+++ webProject/src/main/java/model/Product.java
@@ -0,0 +1,52 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Product {
+    private String title;
+    private int price;
+    private static List<Product> list = new ArrayList<Product>();
+    static {
+        createData();
+    }
+    public Product() {
+    }
+
+    public Product(String title, int price) {
+        this.title = title;
+        this.price = price;
+    }
+
+    public static List<Product> createData() {
+        for (int i = 0; i < 55; i++) {
+            list.add(new Product("Dây nịt cá sấu", 100000));
+        }
+
+        return list;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public static List<Product> getList() {
+        return list;
+    }
+
+    public void setList(List<Product> list) {
+        this.list = list;
+    }
+}
